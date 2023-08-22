22c4f8443ddf3d46fe0ea658bb05936254e248aa
MosesOchieng <mosesochiengopiyo@gmail.com>
2023-08-22 14:00:05 +0300

    first commit

--- frontend/public/Dashboard/app.js
+++ frontend/public/Dashboard/app.js
@@ -21,11 +21,6 @@ async function initContract() {
     const contractAddress = "0xf8e81D47203A594245E36C48e151709F0C19fBe8"; // Replace with your contract address
     const contractAbi = [[
         {
-            "inputs": [],
-            "stateMutability": "nonpayable",
-            "type": "constructor"
-        },
-        {
             "anonymous": false,
             "inputs": [
                 {
@@ -43,6 +38,24 @@ async function initContract() {
             ],
             "name": "Subscribed",
             "type": "event"
+        },
+        {
+            "inputs": [
+                {
+                    "internalType": "uint256",
+                    "name": "packageIndex",
+                    "type": "uint256"
+                }
+            ],
+            "name": "subscribe",
+            "outputs": [],
+            "stateMutability": "payable",
+            "type": "function"
+        },
+        {
+            "inputs": [],
+            "stateMutability": "nonpayable",
+            "type": "constructor"
         },
         {
             "inputs": [],
@@ -99,49 +112,26 @@ async function initContract() {
             ],
             "stateMutability": "view",
             "type": "function"
-        },
-        {
-            "inputs": [
-                {
-                    "internalType": "uint256",
-                    "name": "packageIndex",
-                    "type": "uint256"
-                }
-            ],
-            "name": "subscribe",
-            "outputs": [],
-            "stateMutability": "payable",
-            "type": "function"
         }
-    ] ]; // Replace with your contract ABI
+    ]]; // Replace with your contract ABI
 
     window.insuranceContract = new web3.eth.Contract(contractAbi, contractAddress);
 }
 
 async function displayPackages() {
     const packagesDiv = document.getElementById("packages");
-    const packages = [
-        { name: "Basic Package", price: web3.utils.toWei("1", "ether") },
-        { name: "Standard Package", price: web3.utils.toWei("2", "ether") },
-        { name: "Premium Package", price: web3.utils.toWei("3", "ether") }
-    ];
-    
-    const packageCount = await insuranceContract.methods.getPackageCount().call();
 
-    for (let i = 0; i < packageCount; i++) {
-        const packageDetails = await insuranceContract.methods.insurancePackages(i).call();
-        const packageName = packageDetails.name;
-        const packagePrice = web3.utils.fromWei(packageDetails.price, "ether");
+    const packageNames = ["Basic Package", "Standard Package", "Premium Package"];
 
+    packageNames.forEach(async (packageName, index) => {
         const subscribeButton = document.createElement("button");
-        subscribeButton.innerText = `Subscribe to ${packageName} (${packagePrice} ETH)`;
+        subscribeButton.innerText = `Subscribe to ${packageName}`;
         subscribeButton.style.fontSize = "14px";
-
         subscribeButton.addEventListener("click", async () => {
-            await subscribe(i, packageDetails.price);
+            await subscribe(index);
         });
         packagesDiv.appendChild(subscribeButton);
-    }
+    });
 }
 
 async function subscribe(packageIndex) {
