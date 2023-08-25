c51b475c7c03e8cf91e405c9b57dfc004ed5d782
MosesOchieng <mosesochiengopiyo@gmail.com>
2023-08-25 18:31:00 +0300

    first commit

--- contracts/CommunityInsurancePool.sol
+++ contracts/CommunityInsurancePool.sol
@@ -1,21 +1,19 @@
 // SPDX-License-Identifier: GPL-3.0
-
 pragma solidity >=0.8.1 <0.9.0;
 
 contract CommunityInsurancePoolContract {
-    // Struct to represent an insurance policy
     struct InsurancePolicy {
-        address[] participants; // Addresses of participants in the pool
-        uint256 premiumAmount; // Premium amount in tokens
-        uint256 totalPooledAmount; // Total pooled amount in tokens
-        uint256 claimThreshold; // Minimum pooled amount required to process a claim
-        bool isClaimed; // Whether the policy has been claimed
+        address[] participants;
+        uint256 premiumAmount;
+        uint256 totalPooledAmount;
+        uint256 claimThreshold;
+        bool isClaimed;
+        // Add DAO-related fields here, such as DAO name, etc.
+        // For example: string daoName;
     }
 
-    // Mapping to store insurance policies
     mapping(uint256 => InsurancePolicy) public policies;
 
-    // Event to notify when a new insurance policy is created
     event PolicyCreated(
         uint256 indexed policyId,
         address[] participants,
@@ -24,7 +22,6 @@ contract CommunityInsurancePoolContract {
 
     constructor() {}
 
-    // Function to create a new insurance policy
     function createPolicy(
         address[] memory _participants,
         uint256 _premiumAmount,
@@ -34,17 +31,19 @@ contract CommunityInsurancePoolContract {
             keccak256(abi.encodePacked(block.timestamp, msg.sender))
         );
 
-        // Create the insurance policy
         policies[policyId] = InsurancePolicy({
             participants: _participants,
             premiumAmount: _premiumAmount,
             totalPooledAmount: 0,
             claimThreshold: _claimThreshold,
             isClaimed: false
+            // Initialize the DAO-related fields here
         });
 
         emit PolicyCreated(policyId, _participants, _premiumAmount);
     }
+
+    // Add functions to interact with DAO, such as updating DAO name, etc.
 
     // Function to contribute to an insurance policy
     function contributeToPolicy(uint256 policyId) external payable {
@@ -60,44 +59,5 @@ contract CommunityInsurancePoolContract {
         policies[policyId].totalPooledAmount += msg.value;
     }
 
-    // Function to make a claim from an insurance policy
-    function makeClaim(uint256 policyId) external {
-        InsurancePolicy storage policy = policies[policyId];
-        require(
-            !policy.isClaimed,
-            "CommunityInsurancePoolContract: Policy already claimed"
-        );
-        require(
-            policy.totalPooledAmount >= policy.claimThreshold,
-            "CommunityInsurancePoolContract: Not enough pooled amount for a claim"
-        );
-
-        // Implement the claim evaluation logic based on predefined criteria
-        bool isEligibleForClaim = evaluateClaimEligibility(policyId);
-
-        require(
-            isEligibleForClaim,
-            "CommunityInsurancePoolContract: Not eligible for a claim"
-        );
-
-        // Perform the payout to participants and mark the policy as claimed
-        processClaim(policyId, policy.participants.length);
-
-        policy.isClaimed = true;
-    }
-
-    // Function to evaluate claim eligibility based on predefined criteria
-    function evaluateClaimEligibility(
-        uint256 policyId
-    ) internal view returns (bool) {
-        // Implement your claim eligibility criteria here
-        // Example: Check if the user's health condition meets the criteria for a claim
-        // Return true if eligible, false otherwise
-    }
-
-    // Function to process a successful claim and distribute payouts
-    function processClaim(uint256 policyId, uint256 numParticipants) internal {
-        // Implement the claim payout logic here
-        // Distribute the pooled amount among the participants based on predefined rules
-    }
+    // Add functions for DAO interactions, claim evaluation, and processing payouts
 }
